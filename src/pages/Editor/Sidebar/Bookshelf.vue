40e93e19cb19a1152025a08605ebf87fd81e7edf
ljw1412 <506644791@qq.com>
2023-04-16 20:53:01 +0800

    chore: 新增小说页面基础组件。

--- src/pages/Editor/Sidebar/Bookshelf.vue
+++ src/pages/Editor/Sidebar/Bookshelf.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts" name="SidebarBookshelf">
 import EditorSidebar from '../components/Sidebar.vue'
+
+const actionbar = [
+  { action: 'addVolume', title: '新建书卷', icon: 'icon-folder-add' },
+  { action: 'addChapter', title: '新建章节', icon: 'icon-drive-file' }
+]
+
+function handleActionItemClick(action: string) {}
+
+function addChapter() {}
 </script>
 
 <template>
-  <EditorSidebar class="shadow-xl">
+  <EditorSidebar>
+    <template #extra>
+      <div
+        v-for="item of actionbar"
+        :key="item.action"
+        :title="item.title"
+        class="text-btn w-5 h-5 layout-center rounded cursor-pointer"
+        @click="handleActionItemClick(item.action)"
+      >
+        <component :is="item.icon" :size="16" />
+      </div>
+    </template>
+
     <div class="sidebar-bookshelf"></div>
   </EditorSidebar>
 </template>
